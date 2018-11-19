4c6d833bc8701c60b0aef49323898ee4f05f7017
nonsabotage <nonsabotage@gmail.com>
2018-11-19 20:55:10 +0900

    メッシュサイズの自動読み込み

--- convert_dem5a_to_raster.r
+++ convert_dem5a_to_raster.r
@@ -7,7 +7,7 @@ for (lib in libs[!sapply(libs, require, character.only=TRUE)]) {
     install.packages(lib, repos="https://cran.ism.ac.jp/", depend=TRUE)
     library (lib)
 }
-convert_dem5a_to_raster <- function (ipath, p) {
+convert_dem5a_to_raster <- function (ipath, crs, na_value) {
     coverage  <-
         read_html(ipath, encoding="cp932") %>%
         html_nodes(xpath="//dem//coverage")
@@ -33,25 +33,51 @@ convert_dem5a_to_raster <- function (ipath, p) {
         html_nodes(xpath="//coverage//rangeset//datablock//tuplelist") %>%
         html_text %>%
         read_csv(col_names=c("type", "val"), col_types="cd")
+    gridenvelope_low <-
+    	gridenvelope %>%
+    	html_nodes(xpath="//gridenvelope//low") %>%
+    	html_text() %>%
+    	str_split(" ", simplify = TRUE, n = 2) %>%
+    	parse_integer() %>%
+    	set_names(c("x", "y"))
+    gridenvelope_high <-
+    	gridenvelope %>%
+    	html_nodes(xpath="//gridenvelope//high") %>%
+    	html_text() %>%
+    	str_split(" ", simplify = TRUE, n = 2) %>%
+    	parse_integer() %>%
+    	set_names(c("x", "y"))
+    mesh_size_x <- length(gridenvelope_low["x"]:gridenvelope_high["x"])
+    mesh_size_y <- length(gridenvelope_low["y"]:gridenvelope_high["y"])
+
+
+    # 値は北西方向と南東方向に省略されている可能性があるので
+    # データサイズの調整が必要となる
+    # ex) 海岸線, 河川, 未計測エリア
     vals <- tuplelist$val
-    vals <- c (rep (p$NA_VALUE, length=sp["x"] + sp["y"] * p$MESH_SIZE_X), vals)
-    vals <- c (vals, rep (p$NA_VALUE, length=p$MESH_SIZE_X * p$MESH_SIZE_Y - length(vals)))
+    vals <- c (rep (na_value, length=sp["x"] + sp["y"] * mesh_size_x), vals)
+    vals <- c (vals, rep (na_value, length=mesh_size_x * mesh_size_y - length(vals)))
+    # 北西方向と南東方向のデータ省略ではなく
+    # その間の欠測値については-9999.が記録されているので省略部分と合わせてNAに変換
+    vals[(vals - na_value) < 1.] <- NA
+    # ラスターオブジェクトの作成
     rst  <- raster(xmn=boundedby$lower["x"],
                    xmx=boundedby$upper["x"],
                    ymn=boundedby$lower["y"],
                    ymx=boundedby$upper["y"],
-                   nrows=p$MESH_SIZE_Y,
-                   ncols=p$MESH_SIZE_X,
-                   crs=p$CRS)
-    values(rst) <- vals
-    rst[abs(rst - p$NA_VALUE) <= 1.] <- p$NA_VALUE
+                   nrows=mesh_size_y,
+                   ncols=mesh_size_x,
+                   crs=crs,
+                   vals=vals)
     rst
 }
 writeRasterGrd <- function (r, opath, p) {
-    vmat <- matrix(values(r), nrow=p$MESH_SIZE_Y, ncol=p$MESH_SIZE_X, byrow=TRUE)
+	nr <- dim(r)[1]
+	nc <- dim(r)[2]
+    vmat <- matrix(values(r), nrow=nr, ncol=nc, byrow=TRUE)
     con  <- file(opath, open="wb")
-    cat (sprintf ("ncols %s\n",        ncol(vmat)),      file=con)
-    cat (sprintf ("nrows %s\n",        nrow(vmat)),      file=con)
+    cat (sprintf ("ncols %s\n",        nc),      file=con)
+    cat (sprintf ("nrows %s\n",        nr),      file=con)
     cat (sprintf ("xllcorner %s\n",    extent(r)@xmin),  file=con)
     cat (sprintf ("yllcorner %s\n",    extent(r)@ymin),  file=con)
     cat (sprintf ("cellsize %s\n",     res(r)[1]),       file=con)
--- main.r
+++ main.r
@@ -9,7 +9,8 @@ ipaths <-
     keep( ~ str_detect(.x, "DEM5A"))
 ## xml to grd
 for (ipath in ipaths) {
-    rst <- convert_dem5a_to_raster(ipath, parameters)
+    rst <- convert_dem5a_to_raster(ipath, crs = parameters$CRS, na_value = parameters$NA_VALUE)
+    values(rst) <- ifelse(is.na(values(rst)), parameters$NA_VALUE, values(rst))
     opath <- ipath %>% basename() %>% sub("xml$", "grd", .) %>% file.path(output_dir, .)
     writeRasterGrd(rst, opath, parameters)
 }
